c3fd17d09ef5a80bb5b4db382ee2157a18ccac9b
KazejinFire <wendel.eletrica@hotmail.com>
2017-11-08 20:50:20 -0200

    Initial

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    public float moveSpeed, direction;
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z - player.position.z > 0)
+            direction = -1;
+        else
+            direction = 1;
+        Move(direction, 0.01f);
+        Debug.Log(transform.position.z - player.position.z);
+	}
+
+    private void Move(float dir ,float mSpeed)
+    {
+        float faceVal;
+        transform.position +=new Vector3( 0, 0, dir * mSpeed);
+        if (dir > 0)
+            faceVal = 0;
+        else
+            faceVal = 180;
+        transform.rotation = Quaternion.Euler(0, faceVal, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerControrollerTransform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControrollerTransform : MonoBehaviour {
+    public float moveSpeed;
+    public float horizontalInput, faceVal;
+    public bool isAttacking;
+    public Collider swordCol;
+    Animation anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+        swordCol.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (!isAttacking)
+        {
+            Move(horizontalInput);
+            if (horizontalInput == 0)
+                anim.CrossFade("idle");
+            else
+                anim.CrossFade("Run");
+            if (horizontalInput > 0)
+            {
+                faceVal = 0;
+            }
+            else if (horizontalInput < 0)
+                faceVal = 180;
+            transform.rotation = Quaternion.Euler(0, faceVal, 0);
+        }
+       if (Input.GetButtonDown("Fire1") && !isAttacking)
+        {
+            StartCoroutine(Attack());                   
+        }
+
+    }
+
+    public void Move(float input)
+    {
+        transform.position += new Vector3(0,0,input * moveSpeed);
+    }
+
+    IEnumerator  Attack()
+    {
+        isAttacking = true;
+        anim.Play("Attack");
+        swordCol.enabled = true;
+        yield return new WaitForSeconds(anim["Attack"].length);
+        isAttacking = false;
+        swordCol.enabled = false;
+    }
+}
